bf024e820154dd47034d9efa308a7dc1967ad18f
sergey <honir220@gmail.com>
2020-09-21 15:10:35 +0300

    Update

--- task6.py
+++ task6.py
@@ -1,10 +1,11 @@
-def modify_list(li): 
-	for i in li: 
-		if i % 2 == 1: 
-			li.remove(i)
-	for i in range(len(li)):
-		li[i] = round(li[i] / 2) 
-
-li = [1, 2, 3, 4, 5, 6]
+def modify_list(li):
+	i = 0
+	while i < len(li):
+		if li[i] % 2 == 1:
+			li.pop(i)
+		else:
+			li[i] = round(li[i] / 2)
+			i += 1
+li = [1,2,3,4,5,6,8,8,4,2]
 modify_list(li)
 print(li)
